9c1f415e34542efcbaff0760e6bbe2bafdf0dd3d
olegkamuz@gmail.com <olegkamuz@gmail.com>
2020-01-30 11:37:39 +0200

    Singleton pattern

--- src/singleton/SingletonTest.java
+++ src/singleton/SingletonTest.java
@@ -1,21 +1,19 @@
 package singleton;
 
-import java.util.concurrent.TimeUnit;
-
-public class SingleTest {
+public class SingletonTest {
     public static void main(String... ars) {
         try {
             System.out.println();
             Singleton test = Singleton.getInstance();
             System.out.println(test.sayHi());
-            TimeUnit.SECONDS.sleep(10);
+            Thread.sleep(10);
             Singleton test2 = Singleton.getInstance();
             System.out.println(test2.sayHi());
             System.out.println();
 
             NotASingleton testNoSingleton = new NotASingleton();
             System.out.println(testNoSingleton.sayHi());
-            TimeUnit.SECONDS.sleep(10);
+            Thread.sleep(10);
             NotASingleton testNoSingleton2 = new NotASingleton();
             System.out.println(testNoSingleton2.sayHi());
         } catch (InterruptedException ex) {
